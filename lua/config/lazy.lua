2f399c4924267f035c4459f69f56b1312d752616
lbz <lbzeng96@gmail.com>
2024-01-09 20:30:32 +0800

    Do some adjustments for myself

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,20 +1,25 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+
+-- 检查并安装lazy.nvim插件（如果lazy.nvim插件不存在）
+if not vim.loop.fs_stat(lazypath) then -- 如果lazy.nvim插件不存在
   -- bootstrap lazy.nvim
   -- stylua: ignore
+  -- 克隆lazy.nvim插件
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath) -- lazynvim插件的路径优先被搜索
 
 require("lazy").setup({
-  spec = {
+  spec = { -- 定义需要加载的模块
     -- add LazyVim and import its plugins
+    -- 加载LazyVim中的插件配置
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    -- { import = "lazyvim.plugins.extras.lang.typescript" },
-    -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
+    -- 加载自己定义的插件配置
     { import = "plugins" },
   },
   defaults = {
